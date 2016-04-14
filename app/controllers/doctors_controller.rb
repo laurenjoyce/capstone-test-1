c46923c495764b2ae868dc3d4ecd752d82f178f1
laurenjoyce <joycelc4186@gmail.com>
2016-04-13 22:39:01 -0500

    created test run capstone app, added index page, search for docotrs based on first name and last name

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,24 @@
+class DoctorsController < ApplicationController
+  def index
+    @doctors = []
+    if params[:first_name] && params[:last_name]
+      response_body = Unirest.get("https://api.betterdoctor.com/2016-03-01/doctors?first_name=#{params[:first_name]}&last_name=#{params[:last_name]}&skip=0&limit=100&user_key=#{ENV['better_doctor_api_key']}").body
+      @doctors = response_body["data"]
+      @search_total = @doctors.length
+      @list_number = (@search_total - (@search_total - 1))
+    end
+    render "index.html.erb"
+  end
+
+  def show
+    doctor_id = 
+    render "show.html.erb"
+  end
+  # def run_search
+  #   response_body = Unirest.get("https://api.betterdoctor.com/2016-03-01/doctors?first_name=#{params[:first_name]}&last_name=#{params[:last_name]}&skip=0&limit=100&user_key=#{ENV['better_doctor_api_key']}").body
+  #   @doctors = response_body["data"]
+  #   @search_total = @doctors.length
+  #   @list_number = (@search_total - (@search_total - 1))
+  #   render "index.html.erb"
+  # end
+end
