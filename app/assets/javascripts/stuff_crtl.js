84bb33e1bdd240baa89e183a5e4cdb6f7498bb7f
laurenjoyce <joycelc4186@gmail.com>
2016-05-16 19:34:33 -0500

    added specialty search to index page

---
+++ app/assets/javascripts/stuff_crtl.js
@@ -0,0 +1,10 @@
+/*global angular*/
+(function() {
+  "use strict";
+
+  angular.module("app").controller("stuffCtrl", function($scope) {
+    $scope.message = "Hello";
+
+    window.$scope = $scope;
+  });
+})();  
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,18 +4,47 @@ class DoctorsController < ApplicationController
       redirect_to '/user_profiles'
     else
       @doctors = []
+      @specialty = {
+        "Neurologist" => "Neurologist", 
+        "ear-nose-throat-doctor" => "Otolaryngology",
+        "Internist" => "Internal Medicine",
+        "orthopedic-surgeon" => "Orthopedic Surgery",
+        "urologist" => "Urology",
+        "pediatrician" => "Pediatrics",
+        "obstetrics-gynecologist" => "Obstetrics & Gynecology",
+        "dentist" => "Dentistry",
+        "psychiatrist" => "Psychiatry",
+        "family-practitioner" => "Family Medicine",
+        "cytopathologist" => "Cytopathology",
+        "pathologist" => "Pathology",
+        "interventional-cardiologist" => "interventional cardiology",
+        "cardiologist" => "Cardiovascular Disease",
+        "endocrinologist" => "Endocrinology, Diabetes & Metabolism",
+        "colorectal-surgeon" => "Colon & Rectal Surgery",
+        "general-surgeon" => "Surgery",
+        "plastic-surgery-specialist" => "Plastic Surgery",
+        "hospitalist" => "Hospitalist",
+        "gastroenterologist" => "Gastroenterology",
+        "family-nurse-practitioner" => "Family Nurse Practitioner",
+        "family-psychologist" => "Family Psychology",
+        "ophthalmologist" => "Ophthalmology",
+        "periodontist" => "Periodontics",
+        "geriatric-medicine-doctor" => "Geriatric Medicine"
+      }
+      
       @search_query_string = ""
       params[:first_name] && !params[:first_name].empty? ? @search_query_string += "first_name=#{params[:first_name]}&" : @search_query_string += ""
       params[:last_name] && !params[:last_name].empty? ? @search_query_string += "last_name=#{params[:last_name]}&" : @search_query_string += ""
       params[:gender] && !params[:gender].empty? ? @search_query_string += "gender=#{params[:gender]}&" : @search_query_string += ""
       params[:specialty_uid] && !params[:specialty_uid].empty? ? @search_query_string += "specialty_uid=#{params[:specialty_uid]}&" : @search_query_string += ""
 
-
       unless @search_query_string.empty?
         session[:latitude] && !session[:latitude].empty? ? @search_query_string += "location=#{session[:latitude]},#{session[:longitude]},100&" : @search_query_string += ""
         
         url = "https://api.betterdoctor.com/2016-03-01/doctors?#{@search_query_string}skip=0&limit=100&user_key=#{ENV['better_doctor_api_key']}"
         response_body = Unirest.get(url).body
+
+        p @search_query_string
         @doctors = response_body["data"]      
         if @doctors.nil?
           @search_total = 0
