c0768978093a17b63067bd440bb721433d63202a
laurenjoyce <joycelc4186@gmail.com>
2016-05-19 14:12:13 -0500

    added edit, update, destroy methods to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -20,7 +22,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end  
+
+  def destroy
+    @post.destroy
+    redirect_to @post
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
